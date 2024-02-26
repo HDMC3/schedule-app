c767a5d60e371d74ce2272ac686a83ad3fcecfb6
Hugo Moran <moranh56@gmail.com>
2024-02-26 17:54:28 -0600

    Add dbcontext service

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,4 @@
-
+using Persistence;
 namespace WebAPI;
 
 public class Program
@@ -8,6 +8,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddDatabaseContext(builder.Configuration);
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
