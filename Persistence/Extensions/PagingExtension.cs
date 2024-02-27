26fd312a203513153c8fae7125470237f3fad7e1
Hugo Moran <moranh56@gmail.com>
2024-02-26 23:41:43 -0600

    Add extension to IQueryable

---
+++ Persistence/Extensions/PagingExtension.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductsApp.Data.Extensions;
+
+public static class PagingExtension
+{
+    public static async Task<List<T>> GetPagedAsync<T>(this IQueryable<T> query, int page, int take)
+    {
+        var offset = page - 1 > 0 ? (page - 1) * take : 0;
+
+        var result = await query
+            .Skip(offset)
+            .Take(take)
+            .ToListAsync();
+
+        return result;
+    }
+}
