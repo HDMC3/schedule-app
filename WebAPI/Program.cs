65a03fa51489115c558ad8c78c8cfe1fe3719658
Hugo Moran <moranh56@gmail.com>
2024-02-29 10:45:06 -0600

    Enable swagger

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -27,15 +27,15 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        // if (app.Environment.IsDevelopment())
+        // {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                options.RoutePrefix = string.Empty;
-            });
-        }
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
+        // }
 
         app.UseHttpsRedirection();
 
